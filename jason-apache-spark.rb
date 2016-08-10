32ec6140fe87985c865919080124a5c0ffa47756
Jason Murphy <jason.murphy@omni.sc>
2016-08-10 12:08:02 -0700

    brew tap formula for spark 1.6.2

---
+++ jason-apache-spark.rb
@@ -0,0 +1,22 @@
+class JasonApacheSpark < Formula
+  desc "Engine for large-scale data processing"
+  homepage "https://spark.apache.org/"
+  url "https://www.apache.org/dyn/closer.lua?path=spark/spark-1.6.2/spark-1.6.2-bin-hadoop2.6.tgz"
+  version "1.6.2"
+  head "https://github.com/apache/spark/tree/branch-1.6"
+
+  bottle :unneeded
+
+  def install
+    # Rename beeline to distinguish it from hive's beeline
+    mv "bin/beeline", "bin/spark-beeline"
+
+    rm_f Dir["bin/*.cmd"]
+    libexec.install Dir["*"]
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/spark-shell <<<'sc.parallelize(1 to 1000).count()'"
+  end
+end
